130048aa691483eec235508ede7e3c3fb1b013b2
RaidenSh0wgun <markvincentcb@gmail.com>
2026-03-26 13:39:56 +0800

    commit for pull

--- course/models.py
+++ course/models.py
@@ -18,6 +18,27 @@ class Course(models.Model):
         return self.title
 
 
+# class Enrollment(models.Model):
+#     student = models.ForeignKey(
+#         'user.StudentProfile',
+#         on_delete=models.CASCADE,
+#         related_name="enrollments"
+#     )
+    
+#     course = models.ForeignKey(
+#         Course,
+#         on_delete=models.CASCADE,
+#         related_name="enrollments"
+#     )
+
+#     enrolled_at = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         unique_together = ('student', 'course')
+
+#     def __str__(self):
+#         return f"{self.student} enrolled in {self.course}"
+
 class Enrollment(models.Model):
     student = models.ForeignKey(
         'user.StudentProfile',
@@ -30,7 +51,7 @@ class Enrollment(models.Model):
         on_delete=models.CASCADE,
         related_name="enrollments"
     )
-
+    Passkey = models.CharField(max_length=100, blank=True, null=True)
     enrolled_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
